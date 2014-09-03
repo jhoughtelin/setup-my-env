cdee5d76d87ff219dc55148acd7c519dce3541ae
jhoughtelin <jhoughtelin@findsomehelp.com>
2014-09-03 11:54:55 -0600

    Update setup.sh
    was grabbing the wrong link to my .zshrc

--- setup.sh
+++ setup.sh
@@ -11,12 +11,9 @@ curl -L https://raw.githubusercontent.com/jhoughtelin/vimrc/master/.vimrc > ~/.v
 # Install ZSH
 sudo apt-get install zsh
 
-# Change my shell to ZSH
-chsh -s /bin/zsh
-
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
 
 # get my ZSH Config
-curl -L https://github.com/jhoughtelin/zshrc/blob/master/.zshrc > ~/.zshrc
+curl -L https://raw.githubusercontent.com/jhoughtelin/zshrc/blob/master/.zshrc > ~/.zshrc
